87c21b707404aace71548daa00aa7cfd2b356f92
Your Name <you@example.com>
2022-09-27 19:37:36 +0300

    Save7

--- Program.cs
+++ Program.cs
@@ -1,74 +1,144 @@
-﻿//--------------------------------------------ДЗ к семинару №6--------------------------------------------------------------------------------------------
-//Задача №1. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+﻿//--------------------------------------------ДЗ к семинару №7--------------------------------------------------------------------------------------------
+//Задача №1. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 /*
-int[] Numbers(int num)
+double[,] CreateRandom2dArray()
 {
-    int[] arr = new int[num];
-    
-    for(int i = 0; i < num; i++)
+    Console.Write("Input rows number: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input columns number: ");
+    int cols = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input min possible value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input max possible value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+    double[,] newArray = new double[rows, cols];
+
+    for(int i = 0; i< rows; i++)
     {
-        Console.Write("Введите число :");
-        arr[i] = Convert.ToInt32(Console.ReadLine());
-    }
-
-    return arr;
-}
-void PrintArr(int[] arr)
-{
-
-    Console.Write("[");
-    for(int i=0; i<arr.Length;i++)
-        if(i==arr.Length-1)
-            Console.Write($"{arr[i]} ]");
-        else
-            Console.Write($"{arr[i]}, ");
-}
-
-int CountPositive(int[] arr)
-{
-    int zeroplus = 0;
-    foreach(int x in arr)
-        if(x >= 0) zeroplus++;
     
-    return zeroplus;
+        for(int j = 0; j < cols; j++)
+        {
+            newArray[i,j] = new Random().Next(minValue,maxValue) + new Random().NextDouble();
+        }   
+    }
+    return newArray;
+}   
+void PrintArray(double[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        {
+            for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]:f2}  ");
+        }
+        Console.WriteLine();
+    }
 }
-Console.Write("Введите количество чисел: ");
-int n = Convert.ToInt32(Console.ReadLine());
-
-int[] arr = Numbers(n);
-PrintArr(arr);
-Console.WriteLine($"Количество чисел больше 0 : {CountPositive(arr)}");
+double [,] Array = CreateRandom2dArray();
+PrintArray(Array);
 */
-// Задача №2. Напишите программу, которая найдёт точку пересечения двух прямых, 
-//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//Задача №2.Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
 /*
-Console.Write("введите b1 = ");
-double b1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("введите k1 = ");
-double k1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("введите b2 = ");
-double b2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("введите k2 = ");
-double k2 = Convert.ToInt32(Console.ReadLine());
-if(b1 == b2 && k1 == k2)
+int[,] CreateRandom2dArray()
 {
-    Console.WriteLine("Заданные прямые совпадают");
+    Console.Write("Input rows number: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input columns number: ");
+    int cols = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input min possible value: ");
+    int min = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input max possible value: ");
+    int max = Convert.ToInt32(Console.ReadLine());
+
+    int[,] newArray = new int[rows, cols];
+
+    for(int i = 0; i< rows; i++)
+        for(int j = 0; j < cols; j++)
+            newArray[i,j] = new Random().Next(min, max+1);
+    return newArray;
 }
-if(k1 == k2)
+
+void Show2dArray(int[,] array)
 {
-    Console.WriteLine("Заданные прямые не имеют точки пересечения");
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + "  ");
+        Console.WriteLine();
+
+    }
+    Console.WriteLine();
 }
-else
+void ShowArrayElement(int[,] arr)
 {
-    double Xresult = (b2 - b1) / (k1 - k2);
-    double Yresult = k1 * Xresult + b1;
-    Console.WriteLine($"Координаты точки пересечения ({Xresult}; {Yresult})");
+    Console.Write("Enter the item line number: ");
+    int i = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Enter the column number of the element: ");
+    int j = Convert.ToInt32(Console.ReadLine());
+
+    if(i > arr.GetLength(0) || j > arr.GetLength(1))
+    {
+        Console.WriteLine("There is no such element in the array!");
+    } 
+    else
+     Console.WriteLine($"At this position there is an element: {arr[i-1, j-1]}");
+    Console.WriteLine();
+
 }
+int [,] Array = CreateRandom2dArray();
+Show2dArray(Array);
+ShowArrayElement(Array);
 */
+//Задача №3.Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+/*
+int[,] CreateRandom2dArray()
+{
+    Console.Write("Input rows number: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input columns number: ");
+    int cols = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input min possible value: ");
+    int min = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input max possible value: ");
+    int max = Convert.ToInt32(Console.ReadLine());
 
+    int[,] newArray = new int[rows, cols];
 
+    for(int i = 0; i< rows; i++)
+        for(int j = 0; j < cols; j++)
+            newArray[i,j] = new Random().Next(min, max+1);
+    return newArray;
+}
+void Show2dArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + "  ");
+        Console.WriteLine();
 
-
+    }
+    Console.WriteLine();
+}
+void ArithmeticMean(int[,] arr)
+{
+    for(int j = 0; j < arr.GetLength(1); j++)
+    {
+        double sumStlb = 0;
+        for(int i = 0; i < arr.GetLength(0); i++)
+        {
+            sumStlb = (sumStlb + arr[i, j]);
+        }       
+        double average = sumStlb / arr.GetLength(0);
+        Console.Write ($"{average:f2}  ");
+    }
+}
+int [,] Array = CreateRandom2dArray();
+Show2dArray(Array);
+ArithmeticMean(Array);
+*/
 
 
 
